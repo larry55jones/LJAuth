30de79c4007d7ba4c43645ae52390be232387c46
larry55jones <49727128+larry55jones@users.noreply.github.com>
2021-01-09 13:31:56 -0600

    Create LJAuthAPI project

---
+++ LJAuthAPI/Test/TestController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LJAuthAPI.Test
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        [HttpGet]
+        [Route("{input}")]
+        public string Test(string input)
+        {
+            return input;
+        }
+    }
+}
